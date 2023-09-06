cf70ebd24756b30daae24c9efcef8c99a8296742
Lemnul <andreilemnaru73@gmail.com>
2023-09-06 11:27:54 +0300

    Need to resolve the validation

---
+++ main.js
@@ -0,0 +1,5 @@
+function checkPassword() {
+  let password = document.getElementById("password").value;
+  let confirmPassword = document.getElementById("confirmPassword").value;
+  console.log(password, confirmPassword);
+}
