ce89b73c0c525c3b61f53505cb296b828cd54b23
Lemnul <andreilemnaru73@gmail.com>
2023-09-08 12:26:01 +0300

    Solved password validation

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-function checkPassword() {
+const btn = document.querySelector(".button");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
   let password = document.getElementById("password").value;
   let confirmPassword = document.getElementById("confirmPassword").value;
-  console.log(password, confirmPassword);
-}
+  let message = document.getElementById("error");
+
+  if (password.lenght != 0) {
+    if (password == confirmPassword) {
+      message.textContent = "Passwords match";
+    } else {
+      message.textContent = "Passwords don't match";
+    }
+  }
+});
